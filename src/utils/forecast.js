640ce202acba5be347961ea765508a7b69138d70
santhosh c <santhosh@santhoshs-MacBook-Pro.local>
2019-11-06 15:57:36 +0530

    cosmetic changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 forecast = (lat, long, callback) => {
-    const url = `https://api.darksky.net/forecast/9aa3a88078ae8a349af47c1ffed02c44/${lat},${long}`
+    const url = `https://api.darksky.net/forecast/9aa3a88078ae8a349af47c1ffed02c44/${lat},${long}?units=si`
 
     request({url, json: true}, (error, { body }) => {
         if(error) {
@@ -9,7 +9,9 @@ forecast = (lat, long, callback) => {
         } else if (body.error){
             callback('Not able to find location', undefined)
         } else {
-            callback(undefined, `The temperature is ${body.currently.temperature} and chance of rain is ${body.currently.precipProbability}%`)
+            const stringVal = `The temperature is ${body.currently.temperature} degrees and chance of rain is ${body.currently.precipProbability}%.
+            Daily High is - ${body.daily.data[0].temperatureHigh}, Daily Low is - ${body.daily.data[0].temperatureLow}`
+            callback(undefined, stringVal)
         }
     })
 }
